6e4919014d6300e962caef23f0fdbef19104a536
Kit <dev_kit@icloud.com>
2014-07-17 21:02:39 -0700

    test_2

--- pants.rb
+++ pants.rb
@@ -4,23 +4,20 @@ puts "Hi Greg"
 
 pants = gets.chomp
 
-
-<<<<<<< HEAD
 5.times do
 	puts pants + " :) "
 end
-=======
+
 if pants == 'apple bottom jeans'
   puts 'NO!'
 end
 
-5.times do 
+5.times do
 	puts pants + " :) "
-end 
+end
 
 100.times do |i|
 
   puts "#{i+1} one-thousand"
 
 end
->>>>>>> 374544cf2a10b8e2b77c00a9d3d8064180119a63
